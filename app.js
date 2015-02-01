4e5c6c18afacc3a534bebbb3ceb9fc27466a5692
Stas Kokshin <kostt08@gmail.com>
2015-02-01 19:15:37 +0000

    add theme

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-var koa = require("koa"),
-    route = require("koa-route"),
-    app = module.exports = koa();
+var koa = require("koa");
+var route = require("koa-route");
+var app = module.exports = koa();
+var serve = require("koa-static");
 
+//App configuration
+app.use(serve(__dirname + "/public"));
 //routes
 var homeRoutes = require("./routes/homeRoutes.js");
 app.use(route.get("/", homeRoutes.showHome));
